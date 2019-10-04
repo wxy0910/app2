cba7e1655b0363123ca88478d510372a6fa3c3d8
wxy0910 <1192133118@qq.com>
2019-10-04 16:14:51 +0800

    获取网络数据

--- app/src/main/java/com/example/myapp/Main3Activity.java
+++ app/src/main/java/com/example/myapp/Main3Activity.java
@@ -1,27 +1,118 @@
 package com.example.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Main3Activity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class Main3Activity extends AppCompatActivity implements Runnable{
+
+    private float dollarRate = 0.0f;
+    private float euroRate = 0.0f;
+    private float wonRate = 0.0f;
+    private final String TAG = "Rate";
+    Handler handler;
+    TextView hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rmbtransfer);
+
+        SharedPreferences sharedpreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        dollarRate = sharedpreferences.getFloat("key_dollar", 0.0f);
+        euroRate = sharedpreferences.getFloat("key_euro", 0.0f);
+        wonRate = sharedpreferences.getFloat("key_won", 0.0f);
+
+        hello=(TextView)findViewById(R.id.hello);
+
+        Thread t = new Thread(this);
+        t.start();
+
+        handler =new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if(msg.what==5){
+                    String str=(String)msg.obj;
+                    Log.i(TAG,"handleMessage: getMessage msg = " + str);
+                    hello.setText(str);
+                }
+
+                super.handleMessage(msg);
+            }
+        };
+
     }
     //float dollarRate=0.1445f;
     //float euroRate=0.1227f;
     //float wonRate=168.0538f;
-    float dollarRate,euroRate,wonRate;
+    //float dollarRate,euroRate,wonRate;
+
+
+    public void run() { //在子线程里返回消息
+        Log.i(TAG, "run: run()......");
+        for (int i=1;i<3;i++){
+            Log.i(TAG, "run: i=" + i);
+            try {
+                Thread.sleep(2000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        //获取Msg对象，用于返回主线程
+        Message msg=handler.obtainMessage(5);
+        //msg.what=5
+        msg.obj="Hello from run()";
+        handler.sendMessage(msg);
+        //获取网络数据
+        URL url = null;
+        try {
+            url = new URL("http://www.usd-cny.com/icbc.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG, "run: html=" + html);
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    //将输入流InputStream转换为String
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "gb2312");
+        while (true) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
+    }
+
     public void btndollar(View v) {
         transfer(dollarRate);
     }
@@ -52,32 +143,21 @@ public class Main3Activity extends AppCompatActivity {
     }
     public void btnconfig(View v){
         Intent config = new Intent(this,configActivity.class);
-       // Bundle bdl=new Bundle();
-        //bdl.putFloat("key_dollar",dollarRate);
-        //bdl.putFloat("key_euro",euroRate);
-        //bdl.putFloat("key_won",wonRate);
-        //config.putExtras(bdl);
-        //startActivityForResult(config,1);
-
-        SharedPreferences sp=getSharedPreferences("myrate",Main3Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor=sp.edit();
-        editor.putFloat("key_dollar",dollarRate);
-        editor.putFloat("key_euro",euroRate);
-        editor.putFloat("key_won",wonRate);
-        editor.apply();
+        Bundle bdl=new Bundle();
+        bdl.putFloat("key_dollar",dollarRate);
+        bdl.putFloat("key_euro",euroRate);
+        bdl.putFloat("key_won",wonRate);
+        config.putExtras(bdl);
         startActivityForResult(config,1);
+
     }
     protected void onActivityResult(int requestCode,int resultCode,Intent data){
         if(requestCode==1&&resultCode==2){
-           // Bundle bundle=data.getExtras();
-            //dollarRate=bundle.getFloat("key_dollar",0.1f);
-            //euroRate=bundle.getFloat("key_euro",0.1f);
-            //wonRate=bundle.getFloat("key_won",0.1f);
+            Bundle bundle=data.getExtras();
+            dollarRate=bundle.getFloat("key_dollar",0.1f);
+            euroRate=bundle.getFloat("key_euro",0.1f);
+            wonRate=bundle.getFloat("key_won",0.1f);
 
-            SharedPreferences sharedpreferences=getSharedPreferences("myrate",configActivity.MODE_PRIVATE);
-            dollarRate=sharedpreferences.getFloat("key_dollar",0.0f);
-            euroRate=sharedpreferences.getFloat("key_euro",0.0f);
-            wonRate=sharedpreferences.getFloat("key_won",0.0f);
 
         }
         super.onActivityResult(requestCode,resultCode,data);
@@ -91,18 +171,13 @@ public class Main3Activity extends AppCompatActivity {
 
         // 事件处理代码
        Intent config = new Intent(this,configActivity.class);
-        //Bundle bdl=new Bundle();
-        //bdl.putFloat("key_dollar",dollarRate);
-        //bdl.putFloat("key_euro",euroRate);
-        //bdl.putFloat("key_won",wonRate);
-        //config.putExtras(bdl);
-        //startActivityForResult(config,1);
-        SharedPreferences sp=getSharedPreferences("myrate",Main3Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor=sp.edit();
-        editor.putFloat("key_dollar",dollarRate);
-        editor.putFloat("key_euro",euroRate);
-        editor.putFloat("key_won",wonRate);
-        editor.apply();
+        Bundle bdl=new Bundle();
+        bdl.putFloat("key_dollar",dollarRate);
+        bdl.putFloat("key_euro",euroRate);
+        bdl.putFloat("key_won",wonRate);
+        config.putExtras(bdl);
+        startActivityForResult(config,1);
+
         startActivityForResult(config,1);
     }
 
--- app/src/main/java/com/example/myapp/configActivity.java
+++ app/src/main/java/com/example/myapp/configActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,25 +16,22 @@ public class configActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config);
-        //Intent config=getIntent();
-       // Bundle bdl;
+        Intent config=getIntent();
+        Bundle bdl;
          input1 = findViewById(R.id.dollar_rate_key);
          input2 = findViewById(R.id.euro_rate_key);
          input3 = findViewById(R.id.won_rate_key);
-        // bdl=config.getExtras();
-         //if(bdl!=null){
-           //  newDollar=bdl.getFloat("key_dollar");
-            // newEuro=bdl.getFloat("key_euro");
-             //newWon=bdl.getFloat("key_won");
+         bdl=config.getExtras();
+         if(bdl!=null){
+             newDollar=bdl.getFloat("key_dollar");
+             newEuro=bdl.getFloat("key_euro");
+             newWon=bdl.getFloat("key_won");
              input1.setText(String.valueOf(newDollar));
              input2.setText(String.valueOf(newEuro));
              input3.setText(String.valueOf(newWon));
-         //}
+         }
 
-        SharedPreferences sharedpreferences=getSharedPreferences("myrate",Main3Activity.MODE_PRIVATE);
-        newDollar=sharedpreferences.getFloat("key_dollar",0.0f);
-        newEuro=sharedpreferences.getFloat("key_euro",0.0f);
-        newWon=sharedpreferences.getFloat("key_won",0.0f);
+
     }
 
 
@@ -45,12 +43,12 @@ public class configActivity extends AppCompatActivity {
         newEuro = Float.parseFloat(str2);
         newWon = Float.parseFloat(str3);
         Intent intent=getIntent();
-       // Bundle bdl=new Bundle();
-       // bdl.putFloat("key_dollar",newDollar);
-       // bdl.putFloat("key_euro",newEuro);
-       // bdl.putFloat("key_won",newWon);
-       // intent.putExtras(bdl);
-        SharedPreferences sp=getSharedPreferences("myrate",configActivity.MODE_PRIVATE);
+        Bundle bdl=new Bundle();
+        bdl.putFloat("key_dollar",newDollar);
+        bdl.putFloat("key_euro",newEuro);
+        bdl.putFloat("key_won",newWon);
+        intent.putExtras(bdl);
+        SharedPreferences sp=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor=sp.edit();
         editor.putFloat("key_dollar",newDollar);
         editor.putFloat("key_euro",newEuro);
